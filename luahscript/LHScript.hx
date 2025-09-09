07948d9ef75d5ed8735a3ce92efbb9b044e60ace
Su-Tart <107187521+Ajwyunsx@users.noreply.github.com>
2025-09-09 21:50:55 +0800

    Update LHScript.hx

--- luahscript/LHScript.hx
+++ luahscript/LHScript.hx
@@ -4,5 +4,53 @@ import luahscript.LuaInterp;
 
 class LHScript extends LuaInterp
 {
-  
+  #if sys 
+    //纪念用的
+		Lua_Helper_addCallback("require", function(moduleName:String):Dynamic {
+			if (loadedModules.exists(moduleName)) {
+				return loadedModules.get(moduleName);
+			}
+
+			var filePath = moduleName.split(".").join("/") + ".lua";
+			if (!sys.FileSystem.exists(filePath)) {
+				if (sys.FileSystem.exists("tests/" + filePath)) {
+					filePath = "tests/" + filePath;
+				} else {
+					throw "module '" + moduleName + "' not found";
+				}
+			}
+
+			var content = sys.io.File.getContent(filePath);
+			var parser = new LuaParser().parseFromString(content);
+			var moduleFuncExpr = parser;
+
+			this.globals.set("package", { loaded: loadedModules });
+
+			this.expr(moduleFuncExpr);
+			var mainFunc:Dynamic = this.resolve("globalModules");
+			if (mainFunc == null || LuaCheckType.checkType(mainFunc) != TFUNCTION) {
+				this.globals.remove("package");
+				throw "Module " + moduleName + " did not define a main function.";
+			}
+
+			var callResult = try Reflect.callMethod(null, mainFunc, []) catch(e:haxe.Exception) throw error(ECustom(Std.string(e)));
+			var result = callResult; 
+
+			this.globals.remove("globalModules");
+			this.globals.remove("package");
+
+			if (result == null) {
+				throw "Module " + moduleName + " did not return a value.";
+			}
+
+			loadedModules.set(moduleName, result);
+			return result;
+		});
+		#end
+  }
+  //c
+  public function Lua_Helper_addCallback(func:String, args:Dynamic):Void
+  {
+    return globals.set(func, args);
+  }
 }
