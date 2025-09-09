4acfe0a5724812d3b3e0cfa6e5fbc992aa810489
Su-Tart <107187521+Ajwyunsx@users.noreply.github.com>
2025-09-10 00:41:55 +0800

    公开一下

--- luahscript/LuaInterp.hx
+++ luahscript/LuaInterp.hx
@@ -322,7 +322,7 @@ class LuaInterp {
 		return new LuaAndParams();
 	}
 
-	function expr(e:LuaExpr, isLocal:Bool = false):Dynamic {
+	public function expr(e:LuaExpr, isLocal:Bool = false):Dynamic {
 		this.curExpr = e;
 		switch(e.expr) {
 			case EConst(c):
