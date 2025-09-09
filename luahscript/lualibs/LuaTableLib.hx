eafe53d0d52be07a117c3de8aec03df041d34b8a
Su-Tart <107187521+Ajwyunsx@users.noreply.github.com>
2025-09-09 18:39:22 +0800

    愚蠢的table

--- luahscript/lualibs/LuaTableLib.hx
+++ luahscript/lualibs/LuaTableLib.hx
@@ -52,13 +52,100 @@ class LuaTableLib {
 		return table.remove(pos);
 	}
 
-	@:noCompletion public static function lualib_move(table:LuaTable<Dynamic>, s:Int, e:Int, pos:Int, ?target:LuaTable<Dynamic>):LuaTable<Dynamic> {
-		throw "'table.move' is unfinished";
-		return null;
+	public static function lualib_move(table:LuaTable<Dynamic>, s:Int, e:Int, pos:Int, ?target:LuaTable<Dynamic>):LuaTable<Dynamic> {
+		LuaCheckType.checkTable(table);
+		s = LuaCheckType.checkInteger(s);
+		e = LuaCheckType.checkInteger(e);
+		pos = LuaCheckType.checkInteger(pos);
+		if (target != null) {
+			LuaCheckType.checkTable(target);
+		}
+
+		var sourceTable = table;
+		var targetTableForMove = (target != null) ? target : table;
+
+		if (s > e) {
+			return targetTableForMove;
+		}
+
+		if (s < 1) s = 1;
+		if (pos < 1) pos = 1;
+
+		var moveCount = e - s + 1;
+
+		if (sourceTable == targetTableForMove) {
+			if (pos > s) {
+				var srcIdx = e;
+				var destIdx = pos + moveCount - 1;
+				for (i in 0...moveCount) {
+					targetTableForMove.set(destIdx - i, sourceTable.get(srcIdx - i));
+				}
+			} else if (pos < s) {
+				var srcIdx = s;
+				var destIdx = pos;
+				for (i in 0...moveCount) {
+					targetTableForMove.set(destIdx + i, sourceTable.get(srcIdx + i));
+				}
+			}
+		} else {
+			var srcIdx = s;
+			var destIdx = pos;
+			for (i in 0...moveCount) {
+				targetTableForMove.set(destIdx + i, sourceTable.get(srcIdx + i));
+			}
+		}
+		
+		return targetTableForMove;
 	}
 
-	@:noCompletion public static function lualib_sort(table:LuaTable<Dynamic>, comp:Dynamic):Void {
-		throw "'table.sort' is unfinished";
+	public static function lualib_sort(table:LuaTable<Dynamic>, comp:Dynamic):Void {
+		LuaCheckType.checkTable(table);
+		
+		var arr:Array<Dynamic> = [];
+		for (i in 1...table.nextIndex) {
+			arr.push(table.get(i));
+		}
+
+		if (comp != null) {
+			try {
+				arr.sort(function(a:Dynamic, b:Dynamic):Int {
+					var luaComparisonResult:Dynamic = Reflect.callMethod(null, comp, [a, b]);
+					
+					if (luaComparisonResult == null) {
+						return 1;
+					}
+					
+					var haxeBoolResult:Bool = cast luaComparisonResult;
+					return haxeBoolResult ? -1 : 1;
+				});
+			} catch (e:Dynamic) {
+				throw "Invalid comparator function or execution error in table.sort: " + Std.string(e);
+			}
+		} else {
+			arr.sort(function(a:Dynamic, b:Dynamic):Int {
+				if (a == null && b == null) return 0;
+				if (a == null) return -1; 
+				if (b == null) return 1;
+
+				var typeA = Type.typeof(a);
+				var typeB = Type.typeof(b);
+
+				if (typeA != typeB) {
+					return Reflect.compare(Type.getClassName(Type.getClass(a)), Type.getClassName(Type.getClass(b)));
+				}
+
+				switch (typeA) {
+					case TInt, TFloat: return Reflect.compare(a, b);
+					case TClass(String): return Reflect.compare(a, b);
+					case TBool: return Reflect.compare(a, b);
+					default: return Reflect.compare(Std.string(a), Std.string(b));
+				}
+			});
+		}
+
+		for (i in 0...arr.length) {
+			table.set(i + 1, arr[i]);
+		}
 	}
 
 	static inline function getTableIndex(table:LuaTable<Dynamic>):Int {
