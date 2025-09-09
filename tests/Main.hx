170810fae7b1a656e7a26e8b5cbc05de58552ba6
Su-Tart <107187521+Ajwyunsx@users.noreply.github.com>
2025-09-09 21:20:49 +0800

    Update Main.hx

--- tests/Main.hx
+++ tests/Main.hx
@@ -1,13 +1,13 @@
 package;
 
 import luahscript.LuaParser;
-import luahscript.LuaInterp;
+import luahscript.LHScript;
 import luahscript.exprs.LuaExpr;
 
 class Main {
 	public static function main() {
 		var e = new LuaParser().parseFromString(haxe.Resource.getString("test.lua"));
-		var interp = new LuaInterp();
+		var interp = new LHScript();
 		final f = interp.execute(e);
 		trace(f());
 	}
