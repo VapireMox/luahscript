6529385831c738cd38fefca7afa6649f6d6805ce
Su-Tart <107187521+Ajwyunsx@users.noreply.github.com>
2025-09-10 18:08:05 +0800

    Update Main.hx

--- tests/Main.hx
+++ tests/Main.hx
@@ -11,7 +11,7 @@ class Main {
 		var parser = new LuaParser();
 		var interp = new LHScript(input);
 		interp.execute();
-		trace("add() " + interp.callFunc("add", [1, 1]));
+		trace("add(1, 1) " + interp.callFunc("add", [1, 1]));
 		//trace("LUA_ERROR " + interp.onError);
 	}
 }
