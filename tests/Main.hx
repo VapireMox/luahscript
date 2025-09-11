c45c8fbb3b8f38f641f439b86bbd3ee9b03cf350
Su-Tart <107187521+Ajwyunsx@users.noreply.github.com>
2025-09-11 09:04:06 +0800

    Update Main.hx

--- tests/Main.hx
+++ tests/Main.hx
@@ -10,6 +10,16 @@ class Main {
 		var input:String = haxe.Resource.getString("test.lua");
 		var interp = new LHScript(input);
 		interp.setVar("Haxe", MyHaxeClass);
+
+		//自选两个 不过可以更好的处理print
+	    interp.setPrintHandler(function(line, msg) {
+            trace('LUA PRINT[$line]: $msg');
+        });
+        
+        interp.setErrorHandler(function(err) {
+            trace('LUA ERROR: $err');
+        });
+		
 		interp.execute();
 		trace("add(1, 1) " + interp.callFunc("add", [1, 1]));
 		//trace("LUA_ERROR " + interp.onError);
