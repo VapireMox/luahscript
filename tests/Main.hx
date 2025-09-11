7f04f76249903dbda884021af892ce58b0e2ff49
Su-Tart <107187521+Ajwyunsx@users.noreply.github.com>
2025-09-11 08:55:02 +0800

    Update Main.hx

--- tests/Main.hx
+++ tests/Main.hx
@@ -10,8 +10,24 @@ class Main {
 		var input:String = haxe.Resource.getString("test.lua");
 		var parser = new LuaParser();
 		var interp = new LHScript(input);
+		interp.setVar("Haxe", MyHaxeClass);
 		interp.execute();
 		trace("add(1, 1) " + interp.callFunc("add", [1, 1]));
 		//trace("LUA_ERROR " + interp.onError);
 	}
+}
+class MyHaxeClass {
+    public var greeting:String;
+
+    public function new(greeting:String = "Hello from Haxe!") {
+        this.greeting = greeting;
+    }
+
+    public function sayHello():String {
+        return greeting;
+    }
+
+    public function add(a:Int, b:Int):Int {
+        return a + b;
+    }
 }
